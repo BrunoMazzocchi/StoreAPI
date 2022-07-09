a4d190e0b74f21c78f7ce3a56eb450ddce95864d
Bruno Mazzocchi <bemazzocchi@gmail.com>
2022-07-09 11:23:38 -0600

    Initial commit

---
+++ src/main/java/com/example/store/storeApi/StoreRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.store.storeApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreRestApiApplication.class, args);
+	}
+
+}
